86345d236d4212b838b68de3ff1b5b56e31d77eb
Kirill8769 <tkkrozn@gmail.com>
2023-11-23 11:58:53 +0300

    edit get_transactions func in get_transactions_from_file

--- src/readers.py
+++ src/readers.py
@@ -1,29 +1,11 @@
 import csv
 import json
 import os
+
 import pandas as pd
 
-
-path_file_1 = os.path.join("..", "data", "transactions.csv")
-path_file_2 = os.path.join("..", "data", "transactions_excel.xlsx")
+path_file_1 = os.path.join("data", "transactions.csv")
+path_file_2 = os.path.join("data", "transactions_excel.xlsx")
 print(os.path.isfile(path_file_1))
 print(os.path.isfile(path_file_2))
-
-
-def get_read_csv(filepath: str):
-    format_file = filepath[-4:]
-    print(format_file)
-    if "csv" in format_file:
-        df_csv = pd.read_csv(filepath, delimiter=";", encoding="UTF-8")
-        print(df_csv.columns)
-        transactions_csv = df_csv.to_json(orient="records", force_ascii=False)
-        print(transactions_csv)
-    elif "xlsx" in format_file:
-        df_xlsx = pd.read_excel(filepath)
-        print(df_xlsx.columns)
-        transactions_xlsx = df_xlsx.to_json(orient="records", force_ascii=False)
-        print(transactions_xlsx)
-
-
-get_read_csv(path_file_1)
-get_read_csv(path_file_2)
+print(os.getcwd())
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,13 @@
 import json
 from typing import Any
 
+import pandas as pd
+
 from src.external_api import get_actual_currency
 from src.my_logger import logger
 
 
-def get_transactions_json(filepath: str) -> Any:
+def get_transactions_from_file(filepath: str, csv_delimiter: str = ",") -> Any:
     """
     Читает транзакции из файла JSON и возвращает их в виде списка.
 
@@ -13,12 +15,27 @@ def get_transactions_json(filepath: str) -> Any:
     :return: Список транзакций.
     """
     try:
-        with open(filepath, "r", encoding="UTF-8") as file:
-            transactions = json.load(file)
-            return transactions
+        transactions = []
+        format_file = filepath[-4:]
+        if "json" in format_file:
+            with open(filepath, "r", encoding="UTF-8") as file:
+                transactions = json.load(file)
+        elif "csv" in format_file:
+            df = pd.read_csv(filepath, delimiter=csv_delimiter, encoding="UTF-8")
+            transactions = df.to_json(orient="records", force_ascii=False)
+        elif "xlsx" in format_file:
+            df = pd.read_excel(filepath)
+            transactions = df.to_json(orient="records", force_ascii=False)
+        else:
+            logger.warning("Поддерживаются только следующие форматы файлов: json, csv, xlsx")
+    except FileNotFoundError as file_ex:
+        logger.error(f"{file_ex.__class__.__name__}: {file_ex}")
+    except TypeError:
+        logger.warning("TypeError: Принимается только строковый формат пути к обрабатываемому файлу")
     except Exception as ex:
-        logger.error(f"{ex.__class__.__name__}: {ex}")
-        return []
+        logger.debug(f"{ex.__class__.__name__}: {ex}")
+    finally:
+        return transactions
 
 
 def get_amount_transaction(transaction: dict) -> Any:
